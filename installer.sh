e67d1d6bf38670e2ba4a84370581512329288c27
Pouya abbasian <30899554+pouya-abbasian@users.noreply.github.com>
2017-09-14 16:17:37 +0430

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@ pacmngdl(){
 	read -p "Do you want to continue? [Y/n] " want
 	if [ "$want" == "Y" ] || [ "$want" == "y" ];then
 		sudo apt update
-		sudo apt install namp 
+		sudo apt install nmap 
 	else
 		echo "Abort." ; exit
 	fi
